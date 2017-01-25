a369d4ad0b4375975cdcb6b825ec76d878b3f32b
Alexander Zinchuk <alx.zinchuk@gmail.com>
2017-01-25 17:07:04 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var path = require('path');
+
+var transformEndpoints = require('./lib/transformEndpoints');
+var transformModels = require('./lib/transformModels');
+
+var srcDir = process.cwd();
+
+var parts = [
+    fs.readFileSync(path.join(srcDir, 'header.yml'), 'utf-8'),
+    transformEndpoints(fs.readFileSync(path.join(srcDir, 'endpoints.tinyspec'), 'utf-8')),
+    transformModels(fs.readFileSync(path.join(srcDir, 'models.tinyspec'), 'utf-8'))
+];
+
+fs.writeFileSync(path.join(srcDir, 'swagger.yml'), parts.join(''));
