918dd097776e970f591f2a98930643930e712cb2
Alexander Zinchuk <alx.zinchuk@gmail.com>
2017-02-27 19:05:06 +0100

    Support for generating wonderful HTML with `bootprint`

--- index.js
+++ index.js
@@ -2,16 +2,61 @@
 
 var fs = require('fs');
 var path = require('path');
-
+var YAML = require('yamljs');
 var transformEndpoints = require('./lib/transformEndpoints');
 var transformModels = require('./lib/transformModels');
 
+var TARGET_YAML_FILE = 'swagger.yaml';
+var TARGET_JSON_FILE = 'swagger.json';
+var TARGET_HTML_DIR = './html';
+
+var args = process.argv.slice(2);
 var srcDir = process.cwd();
 
-var parts = [
-    fs.readFileSync(path.join(srcDir, 'header.yml'), 'utf-8'),
+var mode = 'yaml';
+
+if (args.indexOf('--json') != -1 || args.indexOf('-j') != -1) {
+    mode = 'json'
+} else if (args.indexOf('--html') != -1 || args.indexOf('-h') != -1) {
+    mode = 'html'
+}
+
+var yamlSpec = [
+    fs.readFileSync(path.join(srcDir, 'header.yaml'), 'utf-8'),
     transformEndpoints(fs.readFileSync(path.join(srcDir, 'endpoints.tinyspec'), 'utf-8')),
     transformModels(fs.readFileSync(path.join(srcDir, 'models.tinyspec'), 'utf-8'))
-];
+].join('');
 
-fs.writeFileSync(path.join(srcDir, 'swagger.yml'), parts.join(''));
+switch (mode) {
+    case 'yaml':
+        fs.writeFileSync(path.join(srcDir, TARGET_YAML_FILE), yamlSpec);
+        break;
+    case 'json':
+        generateJson(yamlSpec);
+        break;
+    case 'html':
+        var needCleanup = !fs.existsSync(TARGET_JSON_FILE);
+        generateJson(yamlSpec);
+        generateHtml(TARGET_JSON_FILE, TARGET_HTML_DIR)
+            .then(function () {
+                if (needCleanup) {
+                    fs.unlinkSync(TARGET_JSON_FILE);
+                }
+            });
+        break;
+}
+
+function generateJson(yamlSpec) {
+    fs.writeFileSync(
+        path.join(srcDir, TARGET_JSON_FILE),
+        JSON.stringify(YAML.parse(yamlSpec), null, '  ')
+    );
+}
+
+function generateHtml(json, target) {
+    return require('bootprint')
+        .load(require('bootprint-openapi'))
+        .build(json, target)
+        .generate()
+        .then(console.log);
+}
