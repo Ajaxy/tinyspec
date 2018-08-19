8e31058dff613602cd699930600810d10d24943e
Alexander Zinchuk <alx.zinchuk@gmail.com>
2018-08-19 16:12:55 +0300

    Code style refactoring

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ let bootprint;
 let bootprintOpenapi;
 
 try {
-    bootprint = require('bootprint');
-    bootprintOpenapi = require('bootprint-openapi');
+  bootprint = require('bootprint');
+  bootprintOpenapi = require('bootprint-openapi');
 } catch (err) {
 }
 
@@ -29,17 +29,17 @@ const outputDir = argv.output || argv.o || '.';
 let mode = 'help';
 
 if (argv.yaml || argv.y) {
-    mode = 'yaml';
+  mode = 'yaml';
 } else if (argv.json || argv.j) {
-    mode = 'json';
+  mode = 'json';
 } else if (argv.html || argv.h) {
-    mode = 'html';
+  mode = 'html';
 }
 
 switch (mode) {
-    case 'help':
-        console.log(
-            `Usage:
+  case 'help':
+    console.log(
+      `Usage:
 tinyspec [options]
 
 Options:
@@ -49,70 +49,70 @@ Options:
     --output | -o    Path to place generated files
     --add-nulls     Include \`null\` as possible value for non-required fields
     --help          Display this help
-`
-        );
-        break;
-    case 'yaml':
-        fs.writeFileSync(path.join(srcDir, outputDir, TARGET_YAML_FILE), generateYaml());
-        break;
-    case 'json':
-        generateJson(generateYaml(), path.join(srcDir, outputDir, TARGET_JSON_FILE));
-        break;
-    case 'html':
-        if (!bootprint || !bootprintOpenapi) {
-            const { peerDependencies } = require('./package');
-            const installCommand = _.map(peerDependencies, (v, k) => `${k}@${v}`).join(' ');
+`,
+    );
+    break;
+  case 'yaml':
+    fs.writeFileSync(path.join(srcDir, outputDir, TARGET_YAML_FILE), generateYaml());
+    break;
+  case 'json':
+    generateJson(generateYaml(), path.join(srcDir, outputDir, TARGET_JSON_FILE));
+    break;
+  case 'html':
+    if (!bootprint || !bootprintOpenapi) {
+      const { peerDependencies } = require('./package');
+      const installCommand = _.map(peerDependencies, (v, k) => `${k}@${v}`).join(' ');
 
-            console.error(`Please, install peer dependencies first: \`npm install ${installCommand}\``);
-            process.exit(1);
+      console.error(`Please, install peer dependencies first: \`npm install ${installCommand}\``);
+      process.exit(1);
+    }
+
+    const jsonFilePath = path.join(srcDir, TARGET_JSON_FILE);
+    const needCleanup = !fs.existsSync(jsonFilePath);
+
+    generateJson(generateYaml(), jsonFilePath);
+    generateHtml(jsonFilePath, outputDir)
+      .then(() => {
+        if (needCleanup) {
+          fs.unlinkSync(jsonFilePath);
         }
-
-        const jsonFilePath = path.join(srcDir, TARGET_JSON_FILE);
-        const needCleanup = !fs.existsSync(jsonFilePath);
-
-        generateJson(generateYaml(), jsonFilePath);
-        generateHtml(jsonFilePath, outputDir)
-            .then(function () {
-                if (needCleanup) {
-                    fs.unlinkSync(jsonFilePath);
-                }
-            });
-        break;
+      });
+    break;
 }
 
 function generateYaml() {
-    const pattern = path.join(srcDir, '**', '@(*models.tinyspec|*endpoints.tinyspec|header.yaml)');
-    const filePaths = glob.sync(pattern, { ignore: path.join(srcDir, '**/node_modules/**') });
-    const byType = _.groupBy(filePaths, (filePath) => filePath.match(/\w+\.\w+$/)[0]);
-    const header = fs.readFileSync(byType['header.yaml'][0], 'utf-8');
-    const models = byType['models.tinyspec'].map((filePath) => fs.readFileSync(filePath)).join('\n\n');
-    const endpoints = byType['endpoints.tinyspec'].map((filePath) => fs.readFileSync(filePath)).join('\n\n');
+  const pattern = path.join(srcDir, '**', '@(*models.tinyspec|*endpoints.tinyspec|header.yaml)');
+  const filePaths = glob.sync(pattern, { ignore: path.join(srcDir, '**/node_modules/**') });
+  const byType = _.groupBy(filePaths, filePath => filePath.match(/\w+\.\w+$/)[0]);
+  const header = fs.readFileSync(byType['header.yaml'][0], 'utf-8');
+  const models = byType['models.tinyspec'].map(filePath => fs.readFileSync(filePath)).join('\n\n');
+  const endpoints = byType['endpoints.tinyspec'].map(filePath => fs.readFileSync(filePath)).join('\n\n');
 
-    const { addNulls } = argv;
+  const { addNulls } = argv;
 
-    const pathsYaml = YAML.stringify(transformEndpoints(endpoints), Infinity, 2);
-    const definitionsYaml = YAML.stringify(transformModels(models, { addNulls }), Infinity, 2);
+  const pathsYaml = YAML.stringify(transformEndpoints(endpoints), Infinity, 2);
+  const definitionsYaml = YAML.stringify(transformModels(models, { addNulls }), Infinity, 2);
 
-    return [header, pathsYaml, definitionsYaml].join('\n');
+  return [header, pathsYaml, definitionsYaml].join('\n');
 }
 
 function generateJson(yamlSpec, target) {
-    fs.writeFileSync(
-        target,
-        JSON.stringify(YAML.parse(yamlSpec), null, '  ')
-    );
+  fs.writeFileSync(
+    target,
+    JSON.stringify(YAML.parse(yamlSpec), null, '  '),
+  );
 }
 
 function generateHtml(json, target) {
-    return bootprint
-        .load(bootprintOpenapi)
-        .merge({
-            handlebars: {
-                partials: path.join(__dirname, './lib/bootprint_partials')
-            }
-        })
-        .build(json, target)
-        .generate()
-        .then(console.log)
-        .catch(console.error);
+  return bootprint
+    .load(bootprintOpenapi)
+    .merge({
+      handlebars: {
+        partials: path.join(__dirname, './lib/bootprint_partials'),
+      },
+    })
+    .build(json, target)
+    .generate()
+    .then(console.log)
+    .catch(console.error);
 }
--- tests/endpoints/transformEndpoints.test.js
+++ tests/endpoints/transformEndpoints.test.js
@@ -3,19 +3,19 @@ const _ = require('lodash');
 const transformEndpoints = require('../../lib/transformEndpoints');
 
 const tests = [
-    'Basic',
-    'Complex',
-    'Different code styles'
+  'Basic',
+  'Complex',
+  'Different code styles',
 ];
 
 describe('Endpoints to OpenAPI paths', () => {
-    _.forEach(tests, (name) => {
-        const key = _.camelCase(name);
-        const source = fs.readFileSync(`${__dirname}/sources/${key}.endpoints.tinyspec`, { encoding: 'utf-8' });
-        const expectation = require(`${__dirname}/expectations/${key}.paths.json`);
+  _.forEach(tests, (name) => {
+    const key = _.camelCase(name);
+    const source = fs.readFileSync(`${__dirname}/sources/${key}.endpoints.tinyspec`, { encoding: 'utf-8' });
+    const expectation = require(`${__dirname}/expectations/${key}.paths.json`);
 
-        it(name, () => {
-            expect(transformEndpoints(source)).toEqual(expectation);
-        });
+    it(name, () => {
+      expect(transformEndpoints(source)).toEqual(expectation);
     });
+  });
 });
